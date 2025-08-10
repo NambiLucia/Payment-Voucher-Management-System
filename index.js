3a7286fecc570f9682fdd2de01480f18aad9d21c
unknown <nambilucia@gmail.com>
2025-08-10 12:13:03 +0300

    add cors |add log file |

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 import express from 'express'
+import fs from'fs'
+import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import cors from 'cors'
+
+
+
+//routes
+
+
 
 const app =express();
 
+// Recreate __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs','request_logs.txt'), { flags: 'a' })
+
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
 //middle ware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
 
 
 
