7d30d981100e8ffeabc5a0ef9f86055849a32c2c
unknown <nambilucia@gmail.com>
2025-09-19 16:34:44 +0300

    export middleware

--- routes/userroute.js
+++ routes/userroute.js
@@ -1,13 +1,15 @@
 import express from 'express'
 const userRoute =express.Router()
-
 import {getUsers,login,register,updateUserById,deleteUserById} from '../controllers/userController.js'
+import { softDeleteFilter } from '../middleware/softDeleteFilter'
+
+
 userRoute
 .get('/',getUsers)
 .post('/register',register)
 .post('/login',login)
 .patch('/updateUser/:id',updateUserById)
-.delete('/deleteUser/:id',deleteUserById)
+.delete('/deleteUser/:id',softDeleteFilter,deleteUserById)
 
 
 export default userRoute;
