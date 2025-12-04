71ab8e4d2d7fca8103582fa0d4967fc89201f647
unknown <nambilucia@gmail.com>
2025-12-04 10:34:40 +0300

    refactor(voucher route): edit filter voucher endpoint and add validating user token

--- routes/voucherroute.js
+++ routes/voucherroute.js
@@ -2,13 +2,14 @@ import express from 'express'
 const voucherRoute =express.Router()
 import { getVouchers,getVouchersByUserId,getVoucherByVoucherId,getFilteredVouchers } from '../controllers/voucherController.js';
 import { softDeleteFilter } from '../middleware/softDeleteFilter.js';
+import { validateToken } from '../middleware/validateToken'
 
 
 voucherRoute
 .get('/',softDeleteFilter,getVouchers)
 .get('/by-user/:userId/vouchers',getVouchersByUserId)
 .get('/by-id/:id',getVoucherByVoucherId)
-.get('/search-status/:status',getFilteredVouchers)
+.get('/filtered',validateToken,getFilteredVouchers)
 
 
 export default voucherRoute;
