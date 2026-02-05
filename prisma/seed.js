6bbc3936608942f56c0d3ec342ef5f0deed130c3
Lucia Nambi <nambilucia@gmail.com>
2026-02-05 14:36:17 +0300

    fix: fix CI error

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,32 +1,32 @@
-import pkg from "@prisma/client";
-const { PrismaClient } = pkg;
-const prisma = new PrismaClient();
-import bcrypt from "bcrypt";
+// import pkg from "@prisma/client";
+// const { PrismaClient } = pkg;
+// const prisma = new PrismaClient();
+// import bcrypt from "bcrypt";
 
-async function main() {
-  // hash password
-  const hashedPassword = await bcrypt.hash("Test256", 10);
+// async function main() {
+//   // hash password
+//   const hashedPassword = await bcrypt.hash("Test256", 10);
 
-  // upsert ensures user exists, avoids unique constraint errors
-  await prisma.user.upsert({
-    where: { email: "testuser@email.com" },
-    update: {},
-    create: {
-      username: "TestUser",
-      email: "testuser@email.com",
-      password: hashedPassword,
-      role: "ADMIN",
-    },
-  });
+//   // upsert ensures user exists, avoids unique constraint errors
+//   await prisma.user.upsert({
+//     where: { email: "testuser@email.com" },
+//     update: {},
+//     create: {
+//       username: "TestUser",
+//       email: "testuser@email.com",
+//       password: hashedPassword,
+//       role: "ADMIN",
+//     },
+//   });
 
-  console.log("CI test user ready");
-}
+//   console.log("CI test user ready");
+// }
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
